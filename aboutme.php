36d8f3a1dad68c6f17afe1ac0d1217f55291bee9
rexnard <100132176+rexnard@users.noreply.github.com>
2022-08-10 16:46:53 +0800

    Create aboutme.php

---
+++ aboutme.php
@@ -0,0 +1,84 @@
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+    
+<body>
+<script><?php include('index.php'); ?></script>
+    <div id="navbar">
+  <a href="aboutme.php">About me</a>
+  <a href="index.php">Home</a>
+  <img src="images/r.jpg">
+  <div class="dropdown">
+  <button class="dropbtn">Sample Works</button>
+  <div class="dropdown-content">
+      <li>
+    <a class="active" href="http://rexnard.herokuapp.com/CLE1MT/" target="blank">CLE1</a>
+    <a class="active" href="https://rexnard.herokuapp.com/LT2/recipe.html" target="blank">LEARNING TASK 2</a>
+    <a class="active" href="http://rexnard.herokuapp.com/LT3/" target="blank">LEARNING TASK 3</a>
+          <a class="active" href="http://rexnard.herokuapp.com/LT4/" target="blank">LEARNING TASK 4</a>
+          <a href="#">LEARNING TASK 5</a>
+      </li>
+  </div>
+</div>
+</div>
+<div style="padding:15px 15px 2500px;font-size:30px;margin-top:30px;">
+
+    <div id="container">
+    <img src="images/night.gif" width="1248" height="450">
+            <div class="centered">
+                
+                <b>RHEYXS ENDINO FERNANDEZ</b>
+            
+           
+                
+            
+            </div>
+            
+             <div class="top-left">Freshmen <br>
+21 years old
+                 <br>
+Student at University of St. La Salle.<br>
+Went to Colegio de Sto. Tomas - San Carlos City Neg. Occ.
+                 <br>
+FUN FACTS
+<br>
+Loves to play online games such as Valorant,GTAV, and many more.
+                 <br>
+Loves riding a motorcycle and loves to travel.</div>
+              
+                
+        
+        
+        </div>
+              <div class="top-right"><img src="images/Gr6.jpg" width="200" alt="Avatar"></div>
+
+        <img src="images/rar.gif" width="1248" height="85">
+            <div class="bottom-right">
+               </div>
+            
+           
+           
+           
+           
+      
+
+         <img src="images/motor.gif" width="1248" height="450">
+            <div id="text-block">
+                <br>
+            
+              
+            </div>
+        </div>
+        
+    
+  
+   
+  
+    <script type="text/javascript" src="style.js"></script>
+    <div id="content">
+
+    </div>
+</body>
+</html>
